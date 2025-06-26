280979f53a0ce9ed64d64bc22d03d8e22f89369b
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:49:50 +0300

    feat: retention-progress-bar

---
+++ src/app/pages/dashboard/portfolio-goals/retention-progress-bar/retention-progress-bar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from '../../../../components/svg-icon/svg-icon';
+
+@Component({
+  selector: 'app-retention-progress-bar',
+  imports: [SvgIcon],
+  templateUrl: './retention-progress-bar.html',
+  styleUrl: './retention-progress-bar.css'
+})
+export class RetentionProgressBar {
+
+}
