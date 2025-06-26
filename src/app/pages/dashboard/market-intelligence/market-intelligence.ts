7c2edc0c05c2845ed5dca1972425832bb0de8f51
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:49:14 +0300

    feat: market-intelligence

---
+++ src/app/pages/dashboard/market-intelligence/market-intelligence.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-intelligence',
+  imports: [],
+  templateUrl: './market-intelligence.html',
+  styleUrl: './market-intelligence.css'
+})
+export class MarketIntelligence {
+
+}
