82c4b75ada40caf5bc5daec3398c300920482f32
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:49:20 +0300

    feat: my-accounts

---
+++ src/app/pages/dashboard/my-accounts/my-accounts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-accounts',
+  imports: [],
+  templateUrl: './my-accounts.html',
+  styleUrl: './my-accounts.css'
+})
+export class MyAccounts {
+
+}
