d4b83195ec3605403819dae03923ea8375e8e2dc
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:50:24 +0300

    feat: work-queue

---
+++ src/app/pages/dashboard/work-queue/work-queue.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Table } from "../../../components/table/table";
+
+@Component({
+  selector: 'app-work-queue',
+  imports: [Table],
+  templateUrl: './work-queue.html',
+})
+export class WorkQueue {
+
+}
