8feb950c561f618783e64f8af1aae6ee950f7f2b
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:48:53 +0300

    feat: table

---
+++ src/app/components/table/table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from "../svg-icon/svg-icon";
+
+@Component({
+  selector: 'app-table',
+  imports: [SvgIcon],
+  templateUrl: './table.html',
+})
+export class Table {
+
+}
