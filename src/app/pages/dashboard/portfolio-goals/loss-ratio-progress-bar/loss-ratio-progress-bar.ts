630c84fd8ecb3a0888c1d8f362af088b8a99d266
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 00:49:41 +0300

    feat: loss-ratio-progress

---
+++ src/app/pages/dashboard/portfolio-goals/loss-ratio-progress-bar/loss-ratio-progress-bar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from '../../../../components/svg-icon/svg-icon';
+
+@Component({
+  selector: 'app-loss-ratio-progress-bar',
+  imports: [SvgIcon],
+  templateUrl: './loss-ratio-progress-bar.html',
+  styleUrl: './loss-ratio-progress-bar.css'
+})
+export class LossRatioProgressBar {
+
+}
