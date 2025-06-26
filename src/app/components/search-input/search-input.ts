74bb2e51c0908c04615269f0099829b6d4e31801
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 02:59:03 +0300

    feat: search-input

---
+++ src/app/components/search-input/search-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.html',
+  styleUrl: './search-input.css'
+})
+export class SearchInput {
+}
