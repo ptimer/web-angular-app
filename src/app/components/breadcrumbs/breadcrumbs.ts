f243621acff7fb944f1c1ff3c45940e2fee84f82
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 23:01:28 +0300

    feat: breadcrumbs

---
+++ src/app/components/breadcrumbs/breadcrumbs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  imports: [],
+  templateUrl: './breadcrumbs.html',
+  styleUrl: './breadcrumbs.css'
+})
+export class Breadcrumbs {
+
+}
