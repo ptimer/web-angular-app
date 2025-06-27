8ae1008b91e65afa425e4fb3ca1a6cd31aa1f6af
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 23:03:20 +0300

    feat: policies

---
+++ src/app/pages/accounts/policies/policies.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from "../../../components/svg-icon/svg-icon";
+
+@Component({
+  selector: 'app-policies',
+  imports: [SvgIcon],
+  templateUrl: './policies.html',
+  styleUrl: './policies.css'
+})
+export class Policies {
+
+}
