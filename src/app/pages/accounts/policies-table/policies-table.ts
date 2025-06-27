39dd9a5a7fa79d8dc089dacd9490802bf006a49b
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 23:03:12 +0300

    feat: policies-table

---
+++ src/app/pages/accounts/policies-table/policies-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies-table',
+  imports: [],
+  templateUrl: './policies-table.html',
+  styleUrl: './policies-table.css'
+})
+export class PoliciesTable {
+
+}
