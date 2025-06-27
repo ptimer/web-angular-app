b61992c371dec745dfbe3cf8f016c4fd5a4270ee
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 23:02:33 +0300

    feat: compliance-documentation

---
+++ src/app/pages/accounts/compliance-documentation/compliance-documentation.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ArrowLink } from "../../../components/arrow-link/arrow-link";
+import { SvgIcon } from '../../../components/svg-icon/svg-icon';
+
+@Component({
+  selector: 'app-compliance-documentation',
+  imports: [ArrowLink, SvgIcon],
+  templateUrl: './compliance-documentation.html',
+  styleUrl: './compliance-documentation.css'
+})
+export class ComplianceDocumentation {
+
+}
