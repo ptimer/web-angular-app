55ba7519c6d82ce8da7345d28b397bacffdeccd7
Illia Bilous <bilous.job@yahoo.com>
2025-06-27 23:01:58 +0300

    feat: accounts-status

---
+++ src/app/pages/accounts/account-status/account-status.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SvgIcon } from "../../../components/svg-icon/svg-icon";
+
+@Component({
+  selector: 'app-account-status',
+  imports: [SvgIcon],
+  templateUrl: './account-status.html',
+  styleUrl: './account-status.css'
+})
+export class AccountStatus {
+
+}
